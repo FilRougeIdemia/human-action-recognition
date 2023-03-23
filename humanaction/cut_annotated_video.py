a87eb9d5af2d407e56532a80c7f2f235bef7a185
Joffrey Ma <jma-21@tp-5d02-01.enst.fr>
2023-03-23 10:42:48 +0100

    Correct video cutting into chunks according to annotations in case of mode 2

--- humanaction/cut_annotated_video.py
+++ humanaction/cut_annotated_video.py
@@ -26,16 +26,21 @@ def cut_both_video_json(video_file, video_dir, annotation_file, keypoint_dir, vi
         label = row['Label']
         action = row['Action']
         mode = row['Mode']
+        # precise the directories to look for the data because of the messy "mode" thing we did
         if mode == 1:
             mode_kpt_dir = "mmpose-skeleton-predictions"
+            mode_video_dir = None
+            video_to_cut_file = video_file
         else :
             mode_kpt_dir = "mode2"
+            mode_video_dir = "../track_selections"
+            video_to_cut_file = row["Video Name"].replace(".h264", "") # You may need to match the h264, I don't have it in my data
         keypoint_file = row['File Name']
         start_frame = row['temporal_segment_start']  # Start frame number (inclusive)
         end_frame = row['temporal_segment_end']    # End frame number (inclusive)
 
         # prepare the base of outputs name
-        output_name = f"{osp.splitext(video_file)[0]}_segment_{start_frame}_{end_frame}_action_{action}_label_{label}"
+        output_name = f"{osp.splitext(video_to_cut_file)[0]}_segment_{start_frame}_{end_frame}_action_{action}_label_{label}"
         # declare the json output, video output only if json_only False
         output_json = osp.join(json_output_dir, output_name + '.json')
 
@@ -49,8 +54,10 @@ def cut_both_video_json(video_file, video_dir, annotation_file, keypoint_dir, vi
             json_frames = skeleton_file['frames'][start_frame:end_frame]
         else :
             output_video = osp.join(video_output_dir, output_name + '.mp4')
+
+            # Go get the right input video to cut        
             # Open the input video
-            input_video = osp.join(video_dir, video_file)
+            input_video =  osp.join(video_dir, video_to_cut_file) if mode_video_dir is None else osp.join(video_dir, mode_video_dir, video_to_cut_file)
             cap = cv2.VideoCapture(input_video)
 
             # Get video properties
@@ -100,12 +107,12 @@ def cut_both_video_json(video_file, video_dir, annotation_file, keypoint_dir, vi
 
 if __name__ == '__main__':
     annotation_file = '/home/users/jma-21/BGDIA706 - Fil Rouge/human-action-recognition/data/input/annotation/Annotations.csv'
-    video_file = 'VID_20230112_093006.mp4'
+    video_file = '20230112_095742.mp4'
     video_dir = '/home/users/jma-21/BGDIA706 - Fil Rouge/acquisition_sacs/'
     keypoint_dir = '/home/users/jma-21/BGDIA706 - Fil Rouge/kpts'
     video_output_dir = "/home/users/jma-21/BGDIA706 - Fil Rouge/acquisition_sacs_decoupees/videos"
     json_output_dir = "/home/users/jma-21/BGDIA706 - Fil Rouge/acquisition_sacs_decoupees/jsons"
-    json_only = True
-    cut_both_video_json(video_file, video_dir+'jm', annotation_file, keypoint_dir, video_output_dir, json_output_dir, json_only)
-    for specific_dir in ['jm', 'ph', 'pp', 'sg', 'vd', 'wd']:
-        cut_jsons_with_annotations(video_dir+specific_dir, annotation_file, keypoint_dir, json_output_dir)
+    json_only = False
+    cut_both_video_json(video_file, video_dir+'vd', annotation_file, keypoint_dir, video_output_dir, json_output_dir, json_only)
+    #for specific_dir in ['jm', 'ph', 'pp', 'sg', 'vd', 'wd']:
+    #    cut_jsons_with_annotations(video_dir+specific_dir, annotation_file, keypoint_dir, json_output_dir)
