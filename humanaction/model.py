150b9089f482db69c109f9539398adea0251de5c
Joffrey Ma <jma-21@tp-5d02-01.enst.fr>
2023-03-23 07:46:50 +0100

    Integrate acquired video chunks into model training but not yet in full pipeline

--- humanaction/model.py
+++ humanaction/model.py
@@ -2,15 +2,13 @@
 # Declare a LSTM and train it on NTU skeleton action data
 import os
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 import time
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
-
-# setting device on GPU if available, else CPU
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print("device: {}".format(device))
+import torch
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 class HumanActionDataset(Dataset):
@@ -23,35 +21,33 @@ class HumanActionDataset(Dataset):
     - (list) classes: id of the classes to consider.
     """
 
-    # constant that can become arguments
-    data2D_dir = "data/input/mmpose_ntu/"
-    data2D_files = os.listdir(data2D_dir)
-    with open("C:\\Users\\Shadow\\Documents\\Projets\\MastereIA\\Idemia\\human-action-recognition\\data\\actions.txt", 'r') as actions_file:
-        actions = [line.replace('\n', '') for line in actions_file.readlines()]
-        actions_file.close()
-    classes = [5, 6, 7, 8, 14, 24, 30, 32, 42]
-    for i,elem in enumerate(classes):
-        print("class {} : {}".format(i, actions[elem]))
-
-    def __init__(self, data_type:str='2D', data_dir:str=data2D_dir, data_files:list=data2D_files, classes:list=classes, is_train:bool=False):
+    def __init__(self, data_type:str='2D', data_dirs:list=None, data_dirs_files:list=None, _data_files_path:list=None, classes:list=None, actions:list=None, is_train:bool=False):
         self.data_type = data_type
-        self.data_dir = data_dir
+        self.data_dirs = ["data/input/mmpose_ntu/", "data/input/acquisition_sacs/2D"] if data_dirs is None else data_dirs
         self.is_train = is_train
+        self.classes = [5, 6, 7, 8, 14, 24, 30, 32, 42, 61] if classes is None else classes
         if self.is_train:
-            self.data_files = [data_file for data_file in data_files if int(data_file[17:-4])-1 in classes]
+            self.data_dirs_files = [[data_file for data_file in os.listdir(data_dir) if int(data_file[17:-4])-1 in self.classes] for data_dir in self.data_dirs] if data_dirs_files is None else data_dirs_files
         else:
-            self.data_files = os.listdir(data_dir)
-        self.classes = classes
+            self.data_dirs_files = [os.listdir(dir) for dir in self.data_dirs]
+        self._data_files_path = [os.path.join(data_dir, data_dir_file) for data_dir, data_dir_files in zip(self.data_dirs, self.data_dirs_files) for data_dir_file in data_dir_files] if _data_files_path is None else _data_files_path
+        if actions is None:
+            with open("data/actions.txt", 'r') as actions_file:
+                actions = [line.replace('\n', '') for line in actions_file.readlines()]
+                actions_file.close()
+        self.actions = actions
         
+        for i,elem in enumerate(self.classes):
+            print("class {} : {}".format(i, self.actions[elem]))
 
     def __len__(self):
-        return len(self.data_files)
+        return len(self._data_files_path)
 
     def __getitem__(self, idx):
-        tensor = torch.Tensor(np.load(os.path.join(self.data_dir, self.data_files[idx])))
+        tensor = torch.Tensor(np.load(self._data_files_path[idx]))
         tensor = tensor.reshape((tensor.shape[0], 2*17))/1000
         if self.is_train:
-            label = self.classes.index(int(self.data_files[idx][17:-4])-1)
+            label = self.classes.index(int(os.path.basename(self._data_files_path[idx])[17:-4])-1)
         else:
             label = 999 # TODO change that
         return (tensor, label)
@@ -85,7 +81,7 @@ class PadSequence():
         return sequences_padded_begin, lengths, labels
 
 
-def train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset, val_dataset, train_dataloader, val_dataloader):
+def train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset, val_dataset, train_dataloader, val_dataloader, classes, device):
 
     s = time.time()
     train_losses, val_losses, train_accs, val_accs = [], [], [], []
@@ -187,18 +183,34 @@ class ActionLSTM(nn.Module):
         return results, h_n, c_n
 
 
+def stratified_split(dataset: HumanActionDataset, test_size: float):
+    # Extract the labels from the dataset
+    targets = [label for _, label in dataset]
+
+    # Perform stratified splitting using the targets
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=42)
+    train_idx, test_idx = next(sss.split(dataset._data_files_path, targets))
+
+    # Create train and test datasets using the obtained indices
+    train_dataset = torch.utils.data.Subset(dataset, train_idx)
+    test_dataset = torch.utils.data.Subset(dataset, test_idx)
+
+    return train_dataset, test_dataset
+
+
 def main():
     # setting the device as the GPU if available, else the CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("device: {}".format(device))
 
     # Instanciate dataset
-    HAD2D = HumanActionDataset('2D', data2D_dir, data2D_files, classes)
-    train_dataset2D, val_dataset2D = torch.utils.data.random_split(HAD2D, [int(0.85*len(HAD2D)), len(HAD2D)-int(0.85*len(HAD2D))])
-    train_dataloader2D = torch.utils.data.DataLoader(dataset=train_dataset2D, batch_size=32, collate_fn=PadSequence(), shuffle=True)
-    val_dataloader2D = torch.utils.data.DataLoader(dataset=val_dataset2D, batch_size=32, collate_fn=PadSequence(), shuffle=True)
+    HAD2D = HumanActionDataset('2D', is_train=True)
+    #train_dataset2D, val_dataset2D = torch.utils.data.random_split(HAD2D, [int(0.85*len(HAD2D)), len(HAD2D)-int(0.85*len(HAD2D))])
+    train_dataset2D, val_dataset2D = stratified_split(dataset=HAD2D, test_size=0.2)
+    train_dataloader2D = DataLoader(dataset=train_dataset2D, batch_size=32, collate_fn=PadSequence(), shuffle=True)
+    val_dataloader2D = DataLoader(dataset=val_dataset2D, batch_size=32, collate_fn=PadSequence(), shuffle=True)
     # Instanciate model
-    model = ActionLSTM(nb_classes=len(classes), input_size=2*17, hidden_size_lstm=256, hidden_size_classifier=128, num_layers=1, device=device)
+    model = ActionLSTM(nb_classes=len(HAD2D.classes), input_size=2*17, hidden_size_lstm=256, hidden_size_classifier=128, num_layers=1, device=device)
     model.to(device)
     # Declare training parameters
     criterion = nn.CrossEntropyLoss()
@@ -207,8 +219,8 @@ def main():
     nb_epochs = 200
     epoch_print_frequence = 20
     # Train
-    losses_accs_LSTM03D = train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset2D, val_dataset2D, train_dataloader2D, val_dataloader2D)
-    torch.save(model.state_dict(), "C:\\Users\\Shadow\\Documents\\Projets\\MastereIA\\Idemia\\human-action-recognition\\models_saved\\action_lstm_2D.pt")
+    losses_accs_LSTM03D = train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset2D, val_dataset2D, train_dataloader2D, val_dataloader2D, HAD2D.classes, device)
+    torch.save(model.state_dict(), "models_saved/action_lstm_2D_luggage.pt")
 
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
 
@@ -225,7 +237,7 @@ def main():
     ax[1].set_ylabel("accuracy")
 
     plt.legend()
-    plt.savefig("C:\\Users\\Shadow\\Documents\\Projets\\MastereIA\\Idemia\\human-action-recognition\\models_saved\\action_lstm_2D_loss_acc.png")
+    plt.savefig("models_saved/action_lstm_2D_luggage_loss_acc.png")
 
 if __name__ == "__main__":
     main()
