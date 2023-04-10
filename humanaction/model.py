fc068a9ce0ded54959f28d2082b9d6a4b20c4cee
Joffrey Ma <jma-21@gpu5.enst.fr>
2023-04-10 19:10:00 +0200

    Log enable with step intervals, display in tensorboard

--- humanaction/model.py
+++ humanaction/model.py
@@ -16,8 +16,14 @@ from torch.utils.data import WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
 # local import
 from focal_loss import FocalLoss
+from evaluate_training import confusion_matrix, top_k_accuracy
 
-writer = SummaryWriter()
+device_index=1
+if torch.cuda.is_available():
+    torch.cuda.set_device(device_index)
+    device = torch.device(f"cuda:{device_index}")
+    print(f"device cuda:{device_index}")
+
 
 class HumanActionDataset(Dataset):
 
@@ -34,9 +40,11 @@ class HumanActionDataset(Dataset):
         self.data_dim = int(self.data_type[0])
         self.data_dirs = [osp.join("data/input/mmpose_ntu", self.data_type), osp.join("data/input/acquisition_sacs", self.data_type)] if data_dirs is None else data_dirs
         self.is_train = is_train
-        self.classes = [5, 6, 7, 8, 14, 24, 30, 32, 42, 86, 87, 88, 89, 120, 121, 122] if classes is None else classes
+        # To respect the order grab bag, hold bag, leave bag,  put something into a bag, put on bag, take off bag, take something out of a bag
+        # the classes are not in ascending order
+        self.classes = [6, 7, 8, 9, 15, 25, 31, 33, 43, 121, 122, 123, 89, 87, 88, 90] if classes is None else classes
         if self.is_train:
-            self.data_dirs_files = [[data_file for data_file in os.listdir(data_dir) if int(data_file[-7:-4])-1 in self.classes] for data_dir in self.data_dirs] if data_dirs_files is None else data_dirs_files
+            self.data_dirs_files = [[data_file for data_file in os.listdir(data_dir) if int(data_file[-7:-4]) in self.classes] for data_dir in self.data_dirs] if data_dirs_files is None else data_dirs_files
         else:
             self.data_dirs_files = [os.listdir(dir) for dir in self.data_dirs]
         self._data_files_path = [os.path.join(data_dir, data_dir_file) for data_dir, data_dir_files in zip(self.data_dirs, self.data_dirs_files) for data_dir_file in data_dir_files] if _data_files_path is None else _data_files_path
@@ -46,8 +54,10 @@ class HumanActionDataset(Dataset):
                 actions_file.close()
         self.actions = actions
         
+        self.classes_names = dict()
         for i,elem in enumerate(self.classes):
-            print("class {} : {}".format(i, self.actions[elem - 1]))
+            self.classes_names.update({i : self.actions[elem-1]})
+        print(self.classes_names)
 
     def __len__(self):
         return len(self._data_files_path)
@@ -57,7 +67,7 @@ class HumanActionDataset(Dataset):
         normalization = 1000 if self.data_dim==2 else 1
         tensor = tensor.reshape((tensor.shape[0], self.data_dim*17))/normalization
         if self.is_train:
-            label = self.classes.index(int(os.path.basename(self._data_files_path[idx])[-7:-4])-1)
+            label = self.classes.index(int(os.path.basename(self._data_files_path[idx])[-7:-4]))
         else:
             label = 999 # TODO change that
         return (tensor, label)
@@ -91,80 +101,106 @@ class PadSequence():
         return sequences_padded_begin, lengths, labels
 
 
-def train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset, val_dataset, train_dataloader, val_dataloader, classes, device):
-    s = time.time()
-    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+def train_model(model, criterion, optimizer, nb_epochs, epoch_log_frequence, step_log_frequence_train, train_dataset, val_dataset, train_dataloader, val_dataloader, classes, device, writer):
     sm = nn.Softmax(dim=1).to(device)
     step = 0
 
+    # Initialize step interval metrics
+    # later reinitialize after each step interval
+    step_log_loss_train, step_log_acc_train = 0, 0
+    # Initialize/Reinitialize at each epoch
+    epoch_log_loss, epoch_log_acc, epoch_log_obs_cnt = 0, 0, 0 
+
     for epoch in range(nb_epochs):
-        
-        running_loss_train, running_loss_val, running_acc_train, running_acc_val = 0, 0, 0, 0
+        model.train()
 
-        for train in [True, False]:
+        # Iterate over batches
+        for data in train_dataloader:
+            # get skeleton sequences data per batch
+            inputs = data[0].to(device)
+            # get labels per batch
+            labels = torch.zeros((inputs.shape[0], len(classes)))
+            for i in range(inputs.shape[0]):
+                labels[i][int(data[-1][i])] = 1
+            labels = labels.to(device)
 
-            if train:
-                dataloader = train_dataloader
-                model.train()
+            # reset gradients
+            optimizer.zero_grad()
             
-            else:
-                dataloader = val_dataloader
-                model.eval()
+            # collect raw predictions before softmax (logits)
+            outputs_for_loss,_,_ = model(inputs)
+            
+            # apply softmax, calculate loss
+            outputs = sm(outputs_for_loss).to(device)
+            loss = criterion(outputs_for_loss, labels)
 
-            for data in dataloader:
-                
-                inputs = data[0].to(device)
-                labels = torch.zeros((inputs.shape[0], len(classes)))
-                for i in range(inputs.shape[0]):
-                    labels[i][int(data[-1][i])] = 1
-                labels = labels.to(device)
+            loss.backward()
+            optimizer.step()
+            step += 1
+            # Metrics for log
+            # Log to TensorBoard every step_log_frequence for metrics on the train set
+            if(step%step_log_frequence_train==0):
+                # loss for that batch
+                step_log_loss_train = loss.item()
+                # accuracy for that batch
+                step_log_acc_train = int(torch.sum(outputs.argmax(dim=1) == labels.argmax(dim=1)))/len(inputs)
+                writer.add_scalar(f"Loss [Train set][Step interval:{step_log_frequence_train}]", step_log_loss_train, global_step=step)
+                writer.add_scalar(f"Accuracy [Train set][Step interval:{step_log_frequence_train}]", step_log_acc_train, global_step=step)
+                # reset to 0 to compute over the next step interval
+                step_log_loss_train, step_log_acc_train = 0, 0
 
-                if train:
-                    optimizer.zero_grad()
+        # Log to TensorBoard each epoch_log_frequence for metrics on the entire train and validation sets
+        if(epoch%epoch_log_frequence==0):
+            plot_class_distribution(train_dataset, "Train Dataset Class Distribution With Weighted Sampler", writer, "Train_Dataset_Weighted")
+            model.eval()
+            with torch.no_grad():
+                # Go through the data with dataloaders to take advantage of samplers
+                for dataloader in [train_dataloader, val_dataloader]:
+                    dataset_type = 'Train' if dataloader==train_dataloader else 'Validation' 
+                    all_preds = []
+                    all_labels = []
+                    all_outputs = []
+                    for data in dataloader:
+                        inputs = data[0].to(device)
+                        labels = torch.zeros((inputs.shape[0], len(classes)))
+                        for i in range(inputs.shape[0]):
+                            labels[i][int(data[-1][i])] = 1
+                        labels = labels.to(device)
+                        epoch_log_obs_cnt += 1
+                        outputs = model(inputs)[0]
+                        preds = torch.argmax(outputs, dim=1)
+                        loss = criterion(outputs, labels)
+                        epoch_log_loss += loss.item()
+                        epoch_log_acc += int(torch.sum(outputs.argmax(dim=1) == labels.argmax(dim=1)))
+                        all_preds.extend(preds.tolist())
+                        all_labels.extend(data[-1].tolist())
+                        all_outputs.extend(outputs.tolist())
+                    # Calculate loss 
+                    epoch_log_loss /= len(dataloader)
+                    writer.add_scalar(f"Loss [{dataset_type} set][Epoch interval:{epoch_log_frequence}]", epoch_log_loss, global_step=step)
+                    # Calculate accuracy for the entire val_dataset
+                    epoch_log_acc /= epoch_log_obs_cnt
+                    writer.add_scalar(f"Accuracy overall [{dataset_type} set][Epoch interval:{epoch_log_frequence}]", epoch_log_acc, global_step=step)
+                    # Calculate accuracy per class
+                    acc_per_class = dict()
+                    for act_idx, act in train_dataset.dataset.classes_names.items():
+                        all_labels_bin = (np.array(all_labels) == act_idx)
+                        all_preds_bin = (np.array(all_preds) == act_idx)
+                        correct = np.sum(all_labels_bin == all_preds_bin)
+                        acc_per_class.update({act:correct / len(all_labels_bin)})
+                    writer.add_scalars(f"Accuracy per class [{dataset_type} set][Epoch interval:{epoch_log_frequence}]", acc_per_class, global_step=step)
+                    # Calculate confusion matrix for the entire val_dataset
+                    confusion_mat, f1, precision, recall = confusion_matrix(all_outputs, all_labels, normalize=None)
+                    writer.add_scalar(f'F1 overall [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', np.mean(f1), global_step=step)
+                    writer.add_scalars(f'F1 per class [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', {act:f1[act_idx] for act_idx, act in train_dataset.dataset.classes_names.items()}, global_step=step)
+                    writer.add_scalar(f'Precision overall [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', np.mean(precision), global_step=step)
+                    writer.add_scalars(f'Precision per class [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', {act:precision[act_idx] for act_idx, act in train_dataset.dataset.classes_names.items()}, global_step=step)
+                    writer.add_scalar(f'Recall overall [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', np.mean(recall), global_step=step)
+                    writer.add_scalars(f'Recall per class [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', {act:precision[act_idx] for act_idx, act in train_dataset.dataset.classes_names.items()}, global_step=step)
+                    writer.add_figure(f'Confusion [{dataset_type} set][Epoch interval:{epoch_log_frequence}]', confusion_mat, global_step=step)
 
-                outputs_for_loss,_,_ = model(inputs)
-                
-                outputs = sm(outputs_for_loss).to(device)
-                loss = criterion(outputs_for_loss, labels)
-
-                if train:
-                    loss.backward()
-                    optimizer.step()
-                    running_loss_train += loss.item()
-                    running_acc_train += int(torch.sum(outputs.argmax(dim=1) == labels.argmax(dim=1)))
-                else:
-                    running_loss_val += loss.item()
-                    running_acc_val += int(torch.sum(outputs.argmax(dim=1) == labels.argmax(dim=1)))
-                step += 1
-
-        running_loss_train /= len(train_dataloader)
-        running_loss_val /= len(val_dataloader)
-        running_acc_train /= len(train_dataset)
-        running_acc_val /= len(val_dataset)
-
-        train_losses.append(running_loss_train)
-        val_losses.append(running_loss_val)
-        train_accs.append(running_acc_train)
-        val_accs.append(running_acc_val)
-
-        # Log to TensorBoard
-        writer.add_scalar("Loss train", running_loss_train, global_step=step)
-        writer.add_scalar("Loss val", running_loss_val, global_step=step)
-        writer.add_scalar("Accuracy train", running_acc_train, global_step=step)
-        writer.add_scalar("Accuracy val", running_acc_val, global_step=step)
-
-
-        if (epoch == 0) or ((epoch+1) % epoch_print_frequence == 0):
-            print("epochs {} ({} s) | train loss : {} | val loss : {} | train acc : {} | val acc : {}".format(
-                epoch+1,
-                int(time.time()-s),
-                running_loss_train,
-                running_loss_val,
-                running_acc_train,
-                running_acc_val
-            ))
-    
-    return train_losses, val_losses, train_accs, val_accs
+                    # reset to 0 to compute over the next (dataloader, epoch)
+                    epoch_log_loss, epoch_log_acc, epoch_log_obs_cnt = 0, 0, 0
 
 
 class ActionLSTM(nn.Module):
@@ -230,7 +266,13 @@ def plot_class_distribution(dataset, title, writer, tag):
     counter = Counter(labels)
 
     plt.figure()
-    plt.bar(list(counter.keys()), list(counter.values()))
+    bars = plt.bar(list(counter.keys()), list(counter.values()))
+
+    # Add the exact number per bar
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height, str(height), ha='center', va='bottom')
+
     plt.title(title)
     plt.xlabel("Class")
     plt.ylabel("Frequency")
@@ -239,13 +281,15 @@ def plot_class_distribution(dataset, title, writer, tag):
 
     # Log class distribution to TensorBoard
     for i, count in counter.items():
-        writer.add_scalars("Classes distribution", {str(key): value for key, value in dict(counter).items()})
+        writer.add_scalars("Classes distribution", {str(key): value for key, value in dict(counter).items()}, global_step=0)
 
 
 def main():
     # setting the device as the GPU if available, else the CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("device: {}".format(device))
+
+    writer = SummaryWriter()
 
     # Instanciate dataset
     HAD = HumanActionDataset('2D', is_train=True)
@@ -263,8 +307,8 @@ def main():
     train_sampler = WeightedRandomSampler(train_sample_weights, len(train_dataset))
     val_sampler = WeightedRandomSampler(val_sample_weights, len(val_dataset))
 
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=32, collate_fn=PadSequence(), sampler=train_sampler)
-    val_dataloader = DataLoader(dataset=val_dataset, batch_size=32, collate_fn=PadSequence(), sampler=val_sampler)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=24, collate_fn=PadSequence(), sampler=train_sampler)
+    val_dataloader = DataLoader(dataset=val_dataset, batch_size=24, collate_fn=PadSequence(), sampler=val_sampler)
 
     # Instanciate model
     model = ActionLSTM(nb_classes=len(HAD.classes), input_size=HAD.data_dim*17, hidden_size_lstm=256, hidden_size_classifier=128, num_layers=1, device=device)
@@ -276,10 +320,11 @@ def main():
     optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-4)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     nb_epochs = 500
-    epoch_print_frequence = 20
+    epoch_log_frequence = 1
+    step_log_frequence_train = 10
     # Train
-    losses_accs_LSTM03D = train_model(model, criterion, optimizer, nb_epochs, epoch_print_frequence, train_dataset, val_dataset, train_dataloader, val_dataloader, HAD.classes, device)
-    torch.save(model.state_dict(), f"models_saved/action_lstm_{HAD.data_type}_luggage_0406.pt")
+    losses_accs_LSTM03D = train_model(model, criterion, optimizer, nb_epochs, epoch_log_frequence, step_log_frequence_train, train_dataset, val_dataset, train_dataloader, val_dataloader, HAD.classes, device, writer)
+    torch.save(model.state_dict(), f"models_saved/action_lstm_{HAD.data_type}_luggage_0410.pt")
 
     # Graphiques de train
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
@@ -297,7 +342,7 @@ def main():
     ax[1].set_ylabel("accuracy")
 
     plt.legend()
-    plt.savefig("models_saved/action_lstm_{HAD.data_type}_luggage_0406_loss_acc.png")
+    plt.savefig(f"models_saved/action_lstm_{HAD.data_type}_luggage_0410_loss_acc.png")
 
 if __name__ == "__main__":
     main()
